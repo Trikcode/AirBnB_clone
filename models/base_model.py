e416bb9b8d385fc29fa2b4421680ac16f06c5a6c
Selam <84750917+sel6@users.noreply.github.com>
2021-11-14 11:20:36 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,29 +1,42 @@
-#!/usr/bin/python3
-import uuid
-import datetime
+#!/bin/usr/python3
+"""file containing basemodel class"""
+from datetime import datetime
 from models import storage
-class BaseModel:
+import uuid
+
+
+class BaseModel():
+    """Class BaseModel, base model for AirBnB Clone"""
+
     def __init__(self, *args, **kwargs):
-        if(bool(kwargs)):
-            for key in kwargs.keys():
-                if(key != '__class__'):
-                    setattr(self, key, kwargs[key])
-                    self.created_at = datetime.datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f') 
-                    self.updated_at = datetime.datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-        else:
+        """initializes BaseModel"""
+        if kwargs is None or len(kwargs) == 0:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = self.created_at
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             storage.new(self)
+        else:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    time = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, time)
+                elif key != "__class__":
+                    setattr(self, key, value)
+
     def __str__(self):
-        return '[{}] ({}) {}'.format(type(self).__name__, self.id, self.__dict__)    
-   
+        """class str method"""
+        classname = self.__class__.__name__
+        return "[{}] ({}) {}".format(classname, self.id, self.__dict__)
+
     def save(self):
-        self.updated_at = datetime.datetime.now()
+        """updates updated_at with current time"""
+        self.updated_at = datetime.now()
         storage.save()
+
     def to_dict(self):
-        selu = self.__dict__
-        selu['__class__'] = type(self).__name__
-        selu['updated_at'] = self.updated_at.isoformat();
-        selu['created_at'] = self.created_at.isoformat();
-        return self.__dict__
+        """creates a dictionary of BaseModel"""
+        self_dictionary = dict(self.__dict__)
+        self_dictionary['__class__'] = self.__class__.__name__
+        self_dictionary['created_at'] = datetime.isoformat(self.created_at)
+        self_dictionary['updated_at'] = datetime.isoformat(self.updated_at)
+        return self_dictionary
